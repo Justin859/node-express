3cd5a0f5ab12b505583e0ce8f439537fe5f6e54b
Justin859 <justinhammond859@yahoo.com>
2017-10-20 11:28:10 +0200

    added social share jquery plugin.

--- public/scripts/blog_detail.js
+++ public/scripts/blog_detail.js
@@ -8,6 +8,11 @@ $(document).ready(function() {
         });
     };
 
+    $("#share").jsSocials({
+        url: window.location.href,
+        shares: ["twitter", "facebook", "googleplus"]
+    });
+
     $('#topic').upvote({callback: callback});
-    $('#login-tool-tip').tooltip('show')
+    $('#login-tool-tip').tooltip('enable');
 });
