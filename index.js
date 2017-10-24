8f9976acc9b4c736fa78f875cba24a4cab05d721
Justin859 <justinhammond859@yahoo.com>
2017-10-24 13:11:19 +0200

    Changing redirect to page requested from for login.

--- index.js
+++ index.js
@@ -478,7 +478,7 @@ passport.authenticate('google', { successRedirect: 'back',
 
 app.get('/logout', function(request, response) {
   request.logout();
-  response.redirect('/');
+  response.redirect(request.get('back'));
 })
 
 app.get('/event-blogs', function(request, response) {
