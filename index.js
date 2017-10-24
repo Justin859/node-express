b7ab5f03834a392fcb8a09bccb938dfe705632ec
Justin859 <justinhammond859@yahoo.com>
2017-10-24 12:34:47 +0200

    using tmp directory to upload.

--- index.js
+++ index.js
@@ -659,118 +659,85 @@ app.post('/admin/upload-blog', function(request, response) {
       console.log("No files were uploaded.");
     } else {
 
+        for (key in request.files) {
+          if (request.files.hasOwnProperty(key)) {
+            if (request.files[key].name) {
+              var uuid_image_name  =  uuidv1() + "-" + request.files[key].name;
+              request.files[key].mv('/tmp/' + uuid_image_name, function(error) {
+                if (error) {
+                  console.log(error);
+                } 
+              });
+            } else {
+              var uuid_image_name  =  null;
+            }
+            img_srcs.push(uuid_image_name);
+          }
+        }
 
-      async.series([
-        function(callback) {
+        for (i=0; i<img_srcs.length; i++) {
+          if (img_srcs[i] !== null) {
+            var params = {
+              localFile: "/tmp/" + img_srcs[i],
+            
+              s3Params: {
+                Bucket: "rockworthy",
+                Key: "blog_images/" + img_srcs[i],
+              },
+            };
+            var uploader = client.uploadFile(params);
+            uploader.on('error', function(err) {
+              console.error("unable to upload:", err.stack);
+            });
+            uploader.on('progress', function() {
+              console.log("progress", uploader.progressMd5Amount,
+                        uploader.progressAmount, uploader.progressTotal);
+            });
+            uploader.on('end', function() {
+              console.log("done uploading");
+            });
+          }
 
-          async.eachSeries(Object.keys(request.files), function(key, callback){
-            if (request.files.hasOwnProperty(key)) {
-              if (request.files[key].name) {
-                var uuid_image_name  =  uuidv1() + "-" + request.files[key].name;
-                request.files[key].mv('/tmp/' + uuid_image_name, function(error) {
-                  if (error) {
-                    console.log(error);
-                  } 
-                });
+        }
+
+        for (i=0; i<img_srcs.length; i++) {
+          if (img_srcs[i] !== null) { 
+            fs.unlink('./tmp/' + img_srcs[i], function(err) {
+              if (err) {
+                console.log(err)
               } else {
-                var uuid_image_name  =  null;
+                console.log('removed image from server.');
               }
-              img_srcs.push(uuid_image_name);
-              callback(null)
-            }
-          })
-
-          callback(null, 'one')
-        },
-
-        function(callback) {
-
-            pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-              client.query(
-                'INSERT INTO event_blogs(blog_title, author_name, description, content, img_src, img_src2, img_src3, img_src4) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
-                 [blog_data.blog_title,
-                  blog_data.author,
-                  blog_data.description,
-                  blog_data.content,
-                  img_srcs[0],
-                  img_srcs[1],
-                  img_srcs[2],
-                  img_srcs[3]], function(err, result) {
-                  console.log(result);
-                  if (err) {
-                    console.log(err);
-                    return console.log("Error " + err);
-                  } else {
-                    console.log('Event Blog uploaded.')
-                    return response.render('pages/admin/upload_blog', {formErrors: false, successMsg: true});
-                  }
-                done();
-                
-              });
-              pg.end();
-              
-            });
-            callback(null, 'two')
+            })
+            img_srcs[i] = s3link + img_srcs[i];
+          }
         }
-      ])
-      async.series({
-        one: function(callback) {
-          async.eachSeries(img_srcs, function(img_src, callback) {
-            if (img_srcs !== null) {
-              var params = {
-                localFile: "/tmp/" + img_src,
-               
-                s3Params: {
-                  Bucket: "rockworthy",
-                  Key: "blog_images/" + img_src,
-                },
-              };
-              var uploader = client.uploadFile(params);
-              uploader.on('error', function(err) {
-                console.error("unable to upload:", err.stack);
-              });
-              uploader.on('progress', function() {
-                console.log("progress", uploader.progressMd5Amount,
-                          uploader.progressAmount, uploader.progressTotal);
-              });
-              uploader.on('end', function() {
-                console.log("done uploading");
-              });
-            }
-            callback(null);
-          })
-          callback(null, 1);
-        },
-        two: function(callback){
-          console.log("files not deleted.");
-          
-          async.eachSeries(img_srcs, function(img_src, callback) {
-            if (img_src !== null) { 
-              fs.unlink('/tmp/' + img_src, function(err) {
-                if(err) {
-                  console.log(err)
-                } else {
-                  console.log('removed image from server.');
-                }
-              }) 
+        img_srcs.sort()
+      }
 
+      pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query(
+          'INSERT INTO event_blogs(blog_title, author_name, description, content, img_src, img_src2, img_src3, img_src4) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
+          [blog_data.blog_title,
+            blog_data.author,
+            blog_data.description,
+            blog_data.content,
+            img_srcs[0],
+            img_srcs[1],
+            img_srcs[2],
+            img_srcs[3]], function(err, result) {
+            console.log(result);
+            if (err) {
+              console.log(err);
+            } else {
+              console.log('Event Blog uploaded.')
+              response.render('pages/admin/upload_blog', {formErrors: false, successMsg: true});
             }
-              callback(null)
-              img_src = s3link + img_src;
-              
-            });
-          
-
-          callback(null, 2)
-        }
-    }, function(err, results) {
-        // results is now equal to: {one: 1, two: 2}
-        console.log(results)
-    });
+          done();
+        });
+        pg.end();
+      });
     }
-
-
-  }
 });
 
 app.get('/admin/event-blogs', function(request, response) {
